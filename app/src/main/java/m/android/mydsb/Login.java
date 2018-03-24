ef623a3b4d34ac34babb0b5de6d6dafeb6a5d6af
jrheiner <39375864+jrheiner@users.noreply.github.com>
2018-03-24 10:56:06 +0100

    Added project files

--- app/src/main/java/m/android/mydsb/Login.java
+++ app/src/main/java/m/android/mydsb/Login.java
@@ -9,6 +9,5 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        //test
     }
 }
