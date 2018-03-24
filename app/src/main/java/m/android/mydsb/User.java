7deeb17e2000139fb5a62d9630cff1b38e489ffb
jrheiner <39375864+jrheiner@users.noreply.github.com>
2018-03-24 11:09:13 +0100

    Added project files

---
+++ app/src/main/java/m/android/mydsb/User.java
@@ -0,0 +1,13 @@
+package m.android.mydsb;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class User extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_user);
+    }
+}
