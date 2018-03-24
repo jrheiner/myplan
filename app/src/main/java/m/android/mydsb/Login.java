f72a789c5f7763686f1d236af84fb27cfa85cdba
jrheiner <39375864+jrheiner@users.noreply.github.com>
2018-03-24 16:33:07 +0100

    Added login authentication & key request/validation

--- app/src/main/java/m/android/mydsb/Login.java
+++ app/src/main/java/m/android/mydsb/Login.java
@@ -8,6 +8,13 @@ import android.widget.Button;
 import android.widget.EditText;
 import android.widget.Toast;
 
+import com.android.volley.Request;
+import com.android.volley.RequestQueue;
+import com.android.volley.Response;
+import com.android.volley.VolleyError;
+import com.android.volley.toolbox.StringRequest;
+import com.android.volley.toolbox.Volley;
+
 public class Login extends AppCompatActivity {
 
     private EditText username;
@@ -15,11 +22,20 @@ public class Login extends AppCompatActivity {
     private Button button_login;
     int attempt_counter = 5;
     private String message;
+    boolean logged_in;
+    private String api_key;
+    String base_url = "https://iphone.dsbcontrol.de/iPhoneService.svc/DSB/";
+    String auth_url = "authid/";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        if (logged_in) {
+            Intent intent = new Intent("m.android.mydsb.User");
+            startActivity(intent);
+            return;
+        }
         login_auth();
     }
 
@@ -31,28 +47,52 @@ public class Login extends AppCompatActivity {
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
-                        if (username.getText().toString().equals("123") && password.getText().toString().equals("test")) {
-                            Toast.makeText(Login.this, "Login successful!", Toast.LENGTH_SHORT).show();
-                            Intent intent = new Intent("m.android.mydsb.User");
-                            startActivity(intent);
-                            password.setText("");
-                            attempt_counter = 5;
-                        } else {
-                            attempt_counter--;
-                            if (attempt_counter > 1) {
-                                message = attempt_counter + " attempts remaining!";
-                            } else if (attempt_counter == 1) {
-                                message = attempt_counter + " attempt remaining!";
-                            } else {
-                                button_login.setEnabled(false);
-                                message = "Login temporarily disabled!";
-                            }
-                            Toast.makeText(Login.this, "Authentication failed!\n" + message, Toast.LENGTH_SHORT).show();
-                            password.setText("");
-
+                        if (username.getText().length() == 0 || password.getText().length() == 0) {
+                            return;
                         }
+                        request_api_key();
                     }
                 }
         );
+    }
+
+    public void request_api_key() {
+        RequestQueue mRequestQueue;
+        StringRequest mStringRequest;
+
+        mRequestQueue = Volley.newRequestQueue(this);
+        mStringRequest = new StringRequest(Request.Method.GET, base_url + auth_url + username.getText() + "/" + password.getText(), new Response.Listener<String>() {
+            @Override
+            public void onResponse(String response) {
+                api_key = response.replaceAll("\"", "");
+                logged_in = !api_key.equals("00000000-0000-0000-0000-000000000000");
+                if (logged_in) {
+                    Toast.makeText(Login.this, "Login successful!", Toast.LENGTH_SHORT).show();
+                    Intent intent = new Intent("m.android.mydsb.User");
+                    startActivity(intent);
+                    password.setText("");
+                    attempt_counter = 5;
+                } else {
+                    attempt_counter--;
+                    if (attempt_counter > 1) {
+                        message = attempt_counter + " attempts remaining!";
+                    } else if (attempt_counter == 1) {
+                        message = attempt_counter + " attempt remaining!";
+                    } else {
+                        button_login.setEnabled(false);
+                        message = "Login temporarily disabled!";
+                    }
+                    Toast.makeText(Login.this, "Authentication failed!\n" + message, Toast.LENGTH_SHORT).show();
+                    password.setText("");
+
+                }
+            }
+        }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+                Toast.makeText(Login.this, error.toString(), Toast.LENGTH_LONG).show();
+            }
+        });
+        mRequestQueue.add(mStringRequest);
     }
 }
