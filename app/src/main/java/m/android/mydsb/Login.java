a067122af2e7cef5ce54a60563213c7d352c9d01
jrheiner <39375864+jrheiner@users.noreply.github.com>
2018-04-18 21:39:02 +0200

    added saved login state

--- app/src/main/java/m/android/mydsb/Login.java
+++ app/src/main/java/m/android/mydsb/Login.java
@@ -1,11 +1,13 @@
 package m.android.mydsb;
 
 import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ProgressBar;
 import android.widget.Toast;
 
 import com.android.volley.Request;
@@ -26,31 +28,51 @@ public class Login extends AppCompatActivity {
     private String api_key;
     String base_url = "https://iphone.dsbcontrol.de/iPhoneService.svc/DSB/";
     String auth_url = "authid/";
+    private ProgressBar progressBar_login;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        if (logged_in) {
+        if (!getLogged_in()) {
+            login_auth();
+        } else {
             Intent intent = new Intent("m.android.mydsb.User");
             startActivity(intent);
-            return;
         }
-        login_auth();
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+
+        if (!getLogged_in()) {
+            button_login.setEnabled(true);
+            login_auth();
+        } else {
+            Intent intent = new Intent("m.android.mydsb.User");
+            startActivity(intent);
+        }
     }
 
     public void login_auth() {
         username = findViewById(R.id.editText_user);
         password = findViewById(R.id.editText_password);
         button_login = findViewById(R.id.button_login);
+        progressBar_login = findViewById(R.id.progressBar_login);
         button_login.setOnClickListener(
                 new View.OnClickListener() {
                     @Override
                     public void onClick(View view) {
+                        button_login.setEnabled(false);
                         if (username.getText().length() == 0 || password.getText().length() == 0) {
+                            button_login.setEnabled(true);
                             return;
                         }
+                        progressBar_login.setVisibility(View.VISIBLE);
                         request_api_key();
+
                     }
                 }
         );
@@ -59,7 +81,7 @@ public class Login extends AppCompatActivity {
     public void request_api_key() {
         RequestQueue mRequestQueue;
         StringRequest mStringRequest;
-
+        progressBar_login = findViewById(R.id.progressBar_login);
         mRequestQueue = Volley.newRequestQueue(this);
         mStringRequest = new StringRequest(Request.Method.GET, base_url + auth_url + username.getText() + "/" + password.getText(), new Response.Listener<String>() {
             @Override
@@ -68,20 +90,26 @@ public class Login extends AppCompatActivity {
                 logged_in = !api_key.equals("00000000-0000-0000-0000-000000000000");
                 if (logged_in) {
                     Toast.makeText(Login.this, "Login successful!", Toast.LENGTH_SHORT).show();
+                    setLogged_in(true);
+                    setApi_key(api_key);
                     Intent intent = new Intent("m.android.mydsb.User");
                     startActivity(intent);
+                    progressBar_login.setVisibility(View.INVISIBLE);
                     password.setText("");
                     attempt_counter = 5;
                 } else {
                     attempt_counter--;
                     if (attempt_counter > 1) {
                         message = attempt_counter + " attempts remaining!";
+                        button_login.setEnabled(true);
                     } else if (attempt_counter == 1) {
                         message = attempt_counter + " attempt remaining!";
+                        button_login.setEnabled(true);
                     } else {
                         button_login.setEnabled(false);
                         message = "Login temporarily disabled!";
                     }
+                    progressBar_login.setVisibility(View.INVISIBLE);
                     Toast.makeText(Login.this, "Authentication failed!\n" + message, Toast.LENGTH_SHORT).show();
                     password.setText("");
 
@@ -94,5 +122,29 @@ public class Login extends AppCompatActivity {
             }
         });
         mRequestQueue.add(mStringRequest);
+    }
+
+    public void setApi_key(String api_key) {
+        SharedPreferences sp1 = getSharedPreferences("api_key", MODE_PRIVATE);
+        SharedPreferences.Editor ed1 = sp1.edit();
+        ed1.putString("api_key", api_key);
+        ed1.apply();
+    }
+
+    public String getApi_key() {
+        SharedPreferences sp2 = this.getSharedPreferences("api_key", MODE_PRIVATE);
+        return sp2.getString("api_key", null);
+    }
+
+    public void setLogged_in(boolean logged_in) {
+        SharedPreferences sp3 = getSharedPreferences("logged_in", MODE_PRIVATE);
+        SharedPreferences.Editor ed3 = sp3.edit();
+        ed3.putBoolean("logged_in", logged_in);
+        ed3.apply();
+    }
+
+    public boolean getLogged_in() {
+        SharedPreferences sp2 = this.getSharedPreferences("logged_in", MODE_PRIVATE);
+        return sp2.getBoolean("logged_in", false);
     }
 }
--- app/src/main/java/m/android/mydsb/User.java
+++ app/src/main/java/m/android/mydsb/User.java
@@ -1,13 +1,41 @@
 package m.android.mydsb;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 
 public class User extends AppCompatActivity {
+
+
+    private Button button_logout;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_user);
+        logout();
+    }
+
+    public void logout() {
+        button_logout = findViewById(R.id.button_logout);
+        button_logout.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View view) {
+                        setLogged_in(false);
+                        // TODO causes crash when restarting app while being logged in
+                        onBackPressed();
+                    }
+                }
+        );
+    }
+
+    public void setLogged_in(boolean logged_in) {
+        SharedPreferences sp3 = getSharedPreferences("logged_in", MODE_PRIVATE);
+        SharedPreferences.Editor ed3 = sp3.edit();
+        ed3.putBoolean("logged_in", logged_in);
+        ed3.apply();
     }
 }
